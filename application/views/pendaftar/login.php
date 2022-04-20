37f4eb7bdab3e1456377e8fa622cf6c6dafc2be4
fn_terupun <fn.terupun@gmail.com>
2022-04-20 21:19:44 +0900

    update lagii

--- application/views/pendaftar/login.php
+++ application/views/pendaftar/login.php
@@ -19,6 +19,17 @@
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet'>
     <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 
+    <style>
+.bg-blue.py-4.bagianfoter {
+    background-color: #673ab7;
+    box-shadow: 0px 4px 8px 0px #757575;
+    position: fixed;
+    width: 100%;
+    margin: 0;
+    left: 0;
+    bottom: 0;
+}
+    </style>
 </head>
 
 <body class='snippet-body'>
